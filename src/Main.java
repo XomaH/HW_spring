92cbbc52fb3983293a55ffc0e5f94201bf4712eb
XomaH <kympol40@gmail.com>
2024-11-19 09:43:50 +0300

    inition commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         //task 1
-        System.out.println("Hello world!");
+        String firstName = "Ivanov";
+        String middleName = "Ivan";
+        String lastName = "Ivanovich";
+        String fullName = firstName + " " + middleName + " " + lastName;
+        System.out.println("Ф.И.О. сотрудника - " + fullName);
+
+        //task 2
+        String fullName2 = fullName.toUpperCase();
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName2);
+
+        //task 3
+        String fullName3 = "Иванов Семён Семёнович";
+        System.out.println("Данные Ф. И. О. сотрудника — " + fullName3.replace("ё", "е"));
     }
 }
